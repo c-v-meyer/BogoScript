f449455082de71a33a6afc278ebc9223330b537f
Christoph <Christoph@DESKTOP-74E4RH5>
2024-10-06 14:45:04 +0200

    Projektdateien hinzufügen.

---
+++ BogoScript/BogoScript.cpp
@@ -0,0 +1,12 @@
+﻿// BogoScript.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "BogoScript.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
